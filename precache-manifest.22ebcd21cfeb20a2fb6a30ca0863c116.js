fe4ec169116360c416b9cbe17041bb22e1592493
kvaso <aakvasovaa@gmail.com>
2020-08-26 13:06:05 +0300

    Updates

---
+++ precache-manifest.22ebcd21cfeb20a2fb6a30ca0863c116.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc1e09823ce288c3746cd8883c72fa8",
+    "url": "/test-invoice/index.html"
+  },
+  {
+    "revision": "f03ece40ecb6b7675c5c",
+    "url": "/test-invoice/static/css/main.50cfb2f1.chunk.css"
+  },
+  {
+    "revision": "b7e8173ad11ccb7fcce6",
+    "url": "/test-invoice/static/js/2.179d6731.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-invoice/static/js/2.179d6731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03ece40ecb6b7675c5c",
+    "url": "/test-invoice/static/js/main.b0a4addd.chunk.js"
+  },
+  {
+    "revision": "fa12000053faf7892048",
+    "url": "/test-invoice/static/js/runtime-main.70f5bb10.js"
+  }
+]);
